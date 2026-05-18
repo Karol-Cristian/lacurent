84edf70b9fcf0c387e93f40d9d4bc864ef82e606
Karol Cristian Lemnaru <lemnarukarol@gmail.com>
2026-05-19 00:53:37 +0300

    commit

--- js/analiza-casa.js
+++ js/analiza-casa.js
@@ -1,51 +1,152 @@
 const steps =
-document.querySelectorAll(".step")
+document.querySelectorAll(".step");
 
-let current=0
+const nextBtn =
+document.getElementById("nextBtn");
 
-showStep()
+const prevBtn =
+document.getElementById("prevBtn");
+
+const finishBtn =
+document.getElementById("finishBtn");
+
+const progressBar =
+document.getElementById("progressBar");
+
+const stepText =
+document.getElementById("stepText");
+
+const houseForm =
+document.getElementById("houseForm");
+
+
+let current = 0;
+
+
+showStep();
+
 
 function showStep(){
 
-steps.forEach(
-s=>s.classList.remove("active")
-)
+steps.forEach(step=>{
+
+step.classList.remove(
+"active"
+);
+
+});
+
 
 steps[current]
-.classList.add("active")
+.classList.add(
+"active"
+);
 
-document
-.getElementById("stepText")
-.innerText=
-`Pas ${current+1}/${steps.length}`
 
-document
-.getElementById("progressBar")
-.style.width=
-((current+1)/steps.length)*100+"%"
+stepText.innerText =
+
+`Pas ${current+1}/${steps.length}`;
+
+
+progressBar.style.width =
+
+((current+1)/steps.length)*100+"%";
+
+
+
+prevBtn.style.display =
+
+current===0
+?
+"none"
+:
+"block";
+
+
+
+if(
+current===steps.length-1
+){
+
+nextBtn.style.display=
+"none";
+
+finishBtn.style.display=
+"block";
 
 }
+else{
+
+nextBtn.style.display=
+"block";
+
+finishBtn.style.display=
+"none";
+
+}
+
+}
+
+
+
+/* CONTINUĂ */
 
 nextBtn.onclick=()=>{
 
-if(current<steps.length-1){
+if(
+current<
+steps.length-1
+){
 
-current++
+current++;
 
-showStep()
+showStep();
 
 }
 
-}
+};
+
+
+
+/* ÎNAPOI */
 
 prevBtn.onclick=()=>{
 
-if(current>0){
+if(
+current>0
+){
 
-current--
+current--;
 
-showStep()
+showStep();
 
 }
 
-}
+};
+
+
+
+/* SUBMIT */
+
+houseForm.addEventListener(
+"submit",
+function(e){
+
+e.preventDefault();
+
+
+alert(
+"Analiza începe ⚡"
+);
+
+
+/*
+aici ulterior:
+
+- trimite API
+- salvează DB
+- generează recomandări
+- redirect dashboard
+*/
+
+});
