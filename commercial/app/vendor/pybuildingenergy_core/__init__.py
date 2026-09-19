df3c489380960e59e0f6de960711d17b4e91b077
Karol Cristian Lemnaru <75829886+Karol-Cristian@users.noreply.github.com>
2026-09-20 00:34:57 +0300

    Add pyBuildingEnergy vendor metadata

---
+++ commercial/app/vendor/pybuildingenergy_core/__init__.py
@@ -0,0 +1,9 @@
+"""Minimal vendored pyBuildingEnergy core used by the Cloudflare compatibility PoC.
+
+Upstream: https://github.com/EURAC-EEBgroup/pyBuildingEnergy
+Pinned source version during PoC: 2.0.3
+License: BSD-3-Clause (see LICENSE.md)
+"""
+
+PBE_UPSTREAM_VERSION = "2.0.3"
+PBE_UPSTREAM_PRIMARY_ENERGY_BLOB = "0129d346b8e8fce4f6f426d9d383878887c405d9"
